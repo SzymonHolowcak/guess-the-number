1ca64e9d35ea11637aafd1c8176b3586feb688f7
MohammedBrunfinger <szholowczak@gmail.com>
2020-11-02 14:52:27 +0100

    wow

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Read instructions. And remove this line.
+function randomInt() {
+    return Math.ceil(Math.random()*100)
+}
+
+document.addEventListener('keyup', function (event) {
+    if (event.key === 'Enter') {
+        const guess = getUserGuess()
+        console.log(guess)
+        
+    }
+})
+
+function getUserGuess() {
+    const stringNum = document.getElementById('userInput').value
+    return parseInt(stringNum)
+
+}
+console.log(randomInt())
