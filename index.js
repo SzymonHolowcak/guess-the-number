01c9ac1039fc983ae282e609fbf2e83849a848ff
MohammedBrunfinger <szholowczak@gmail.com>
2020-11-02 15:42:00 +0100

    det funkar

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 function randomInt() {
     return Math.ceil(Math.random()*100)
 }
-
+const randomNumber = randomInt()
 document.addEventListener('keyup', function (event) {
     if (event.key === 'Enter') {
-        const guess = getUserGuess()
-        console.log(guess)
-        
+        if (randomNumber == getUserGuess()){
+            setMessage("You got it!")
+        } else if (randomNumber < getUserGuess()){
+            setMessage("Too high!")
+        }
+        else{
+            setMessage("Too low!")
+        }
     }
-})
-
+}
+    )
+    console.log(randomNumber)
 function getUserGuess() {
     const stringNum = document.getElementById('userInput').value
     return parseInt(stringNum)
-
 }
-console.log(randomInt())
+function setMessage (str){
+    document.getElementById('message').innerText = str
+}
